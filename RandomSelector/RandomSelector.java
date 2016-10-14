3e09dcec0118ee7eb63991c069ab6cc433620f5f
doerlbh <doerlbh@gmail.com>
2016-10-13 19:42:26 -0700

    update code

---
+++ RandomSelector/RandomSelector.java
@@ -0,0 +1,19 @@
+// Copyright: Baihan Lin, doerlbh@gmail.com
+// Date: Oct 2016
+
+import java.io.*;     // for File, FileNotFoundException
+import java.util.*;   // for Scanner, List, Set, Collections
+
+
+public class RandomSelector {
+
+	public RandomSelector() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
