df5269a6cbf9cbb3fc66bbd3147801a7c28efc92
doerlbh <doerlbh@gmail.com>
2016-10-20 21:50:59 -0700

    update code

--- M381/lpGenerator.pl
+++ M381/lpGenerator.pl
@@ -6,23 +6,40 @@
 #
 # A perl script to generate an lpsolve-format IP (integer program) 
 
-$boardSizeX=4;
-$boardSizeY=4;
+$max=12;
 
 ## print out objective function
 print "min:";
-for ($j=0; $j<$boardSizeY; $j++) {
-	for($i=0; $i<$boardSizeX; $i++) {
+	for($i=0; $i<$max; $i++) {
+		print "+y_",$i;
+	}
+print ";\n";
+
+## constraint 1
+for($j=0; $j<$max; $j++) {
+	for($i=0; $i<$max; $i++) {
 		print "+x_",$j,"_",$i;
 	}
+	print "=1;\n"
 }
-print ";\n";
-		
-## generate a constraint for each square on the board that
-## ensures that the square is attacked
 
+## constraint 2
+for($j=0; $j<$max; $j++) {
+	for($i=0; $i<$max; $i++) {
+		print "x_",$j,"_",$i," -", "y_",$i,"<=0;\n";
+	}
+}
+
+## constraint 3
 for($j=0; $j<$boardSizeY; $j++) {
 	for($i=0; $i<$boardSizeX; $i++) {
+
+		for($c=2;$c<=$n-1;$c++) {
+			if($n%$c==0)
+
+
+
+
 		
 		## find all the locations from which (i,j) could be attacked;
 		## add each one to the constraint for (i,j): (i,j) must be attacked!
@@ -55,10 +72,15 @@ for($j=0; $j<$boardSizeY; $j++) {
  	}
  }
 
+
 ## specify that all variables are binary
 print "bin ";
-for($j=0; $j<$boardSizeY; $j++) {
-	for($i=0; $i<$boardSizeX; $i++) {
+for($i=0; $i<$max; $i++) {
+	if ($i>0) { print ","; } 
+	print "+y_",$i;
+}
+for($j=0; $j<$max; $j++) {
+	for($i=0; $i<$max; $i++) {
 	if ($i+$j>0) { print ","; } ## prepend a comma after the first variable
 		print "x_",$j,"_",$i;
 	}
