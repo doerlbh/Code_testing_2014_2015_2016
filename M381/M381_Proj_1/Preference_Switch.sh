b4d57d2ea06e0ea6a4c030db3c306cbfc1f0f945
doerlbh <doerlbh@gmail.com>
2016-10-26 13:12:31 -0700

    update code

--- M381/M381_Proj_1/Preference_Switch.sh
+++ M381/M381_Proj_1/Preference_Switch.sh
@@ -4,44 +4,16 @@
 
 #cat rslist | uniq -d > rslistuniq;
 
+cp pref.list pref.run;
+sed -i -e 's/^/if [ $j = /' pref.run;
+sed -i 's#$# ];then echo #' pref.run;
 
+paste pref.run prefMF.list > pref_ind_switch.sh;
+sed -i 's#$# >> new_pref.out;fi#' pref_ind_switch.sh;
 
+cat pref_ind_switch.sh | tr -d '\011' > pref_ind_switch_final.sh;
 
 for j in `cat FoodGroup.list`; do
-	echo $j > temp
-
-
-
-	for i in *out.txt; do
-		grep "A"  $i > ${i//out.txt/PMID};
-		rm *out.txt;
-		#echo anotherone;
-	done
-
-	
-
-
-sed -i '' 's/.*PMID/PMID/g' *PMID*;
-	sed -i '' 's/.*PMID/PMID/g' *PMID*;
-	sed -i '' "s#</a><a href=\"/index.php/File:OA-icon.png\" class=\"image\"><img alt=\"OA-icon.png\" src=\"https://media.snpedia.com/images/5/5b/OA-icon.png\" width=\"15\" height=\"15\" />#$(printf '\t')#g" *PMID*;
-	sed -i '' "s#</a><a href=\"/index.php/File:OA-icon.png\" class=\"image\"><img alt=\"OA-icon.png\" src=\"https://media.snpedia.com/images/5/5b/OA-icon.png\" width=\"15\" height=\"15\" /> #$(printf '\t')#g" *PMID*;
-	sed -i '' "s#</a>]#$(printf '\t')#g" *PMID*;
-	sed -i -e "s/^/$j$(printf '\t')/" *PMID*;
-
+	sh pref_ind_switch_final.sh
 done
-
-#for k in *e; do
-#	cat $k ${k//-e/} > ${k//-e/.combo};
-#	rm $k;
-#done
-
-cat Rs* >> PMIDlist$now;
-sort PMIDlist$now > PMID$now.sort;
-uniq -d PMID$now.sort > PMID$now.uniq;
-
-grep "Rs"  PMID$now.uniq > PMIDRs$now.uniq
-
-#cat *PMID.combo >> PMIDlist$now;
-rm Rs*;
-
 
--- M381/M381_Proj_1/test.sh
+++ M381/M381_Proj_1/test.sh
@@ -1,5 +1,3 @@
 for j in `cat FoodGroup.list`; do
-	if [ $j = 'BH' ] 
-		then echo BH 3.5 5.4;
-	 fi
+	if [ $j = BH ];then echo BH 3.5 5.4;fi
 done
