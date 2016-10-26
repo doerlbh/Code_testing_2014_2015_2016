d5e4d3d33f3eaab2bae52ea84cb62497f3d1f974
doerlbh <doerlbh@gmail.com>
2016-10-26 15:00:17 -0700

    update code

--- M381/M381_Proj_1/test.sh
+++ M381/M381_Proj_1/test.sh
@@ -1,3 +1,28 @@
-for j in `cat FoodGroup.list`; do
-	if [ $j = BH ];then echo BH 3.5 5.4;fi
-done
+#!/usr/local/bin/
+# Baihan Lin
+# October 2016
+
+#cat rslist | uniq -d > rslistuniq;
+
+rm pref*.run*;
+rm pref_ind_switch.sh*;
+rm pref_ind_switch_final.sh*;
+rm new_pref.out*;
+
+cp pref.list pref1.run;
+sed -i -e 's/^/elif [[ $j = /' pref1.run;
+
+cp prefMF.list pref2.run;
+sed -i -e 's/^/ ]];then echo /' pref2.run;
+
+paste pref1.run pref2.run > pref_ind_switch.sh;
+sed -i '' 's#$# >> new_pref.out;#' pref_ind_switch.sh;
+#sed -i '' 's#$#;fi;#' pref_ind_switch.sh;
+
+#sed -i -e "s/$(print '\t')/ /" pref_ind_switch.sh;
+
+cat pref_ind_switch.sh | tr -d '\011' > pref_ind_switch_final.sh;
+#cat pref_ind_switch.sh > pref_ind_switch_final.sh;
+
+sed -i '' "s/+/ /" pref_ind_switch_final.sh;
+sed -i '' "s/+/ /" pref_ind_switch_final.sh;
