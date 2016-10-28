8516639dbe4fcf8e7db7a55cb16a1a5925d91ac6
doerlbh <doerlbh@gmail.com>
2016-10-28 02:20:33 -0700

    update code

---
+++ AMATH531/A531_HW1.m
@@ -0,0 +1,8 @@
+%% HW2 for MATH381
+% @Author: Baihan Lin
+% @Date: Oct 2016
+
+clear all; close all; clc;
+
+%% Question 1
+
---
+++ AMATH531/A531_HW3.m
@@ -0,0 +1,24 @@
+%% HW3 for MATH531
+% @Author: Baihan Lin
+% @Date: Oct 2016
+
+clear all; close all; clc;
+
+%% Question 1
+
+% part b)
+
+V = 100;
+
+alpha = 3*c^(-2)*t^(-1);
+beta = 0.6*c^(-2)*t^(-1);
+lamda = 2.95*t^(-1);
+delta = 0.25*t^(-1);
+
+a = 1*c;
+b = 1.5*c;
+
+
+
+
+% part c)
