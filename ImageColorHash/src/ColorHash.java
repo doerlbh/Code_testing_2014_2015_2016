3414e0a67e76d6444892508e1b358152437f32c0
doerlbh <doerlbh@gmail.com>
2016-10-30 13:38:30 -0700

    update code

--- ImageColorHash/src/ColorHash.java
+++ ImageColorHash/src/ColorHash.java
@@ -1,11 +1,24 @@
-/**
- * @author (put your name here)
+import java.awt.image.BufferedImage;
+
+/** ColorHash.java
+ * 
+ * by Baihan Lin
+ * for CSE 373 Assignment 3, Autumn, 2016.
+ * Section BC. 
+ * 
+ * @author Baihan Lin  < sunnylin@uw.edu >
+ * @version 	1.2
+ * @since 		1.0 2016-10-25
  *
  */
+
 public class ColorHash {
 	// Implement this class, including whatever data members you need and all
 	// of the public methods below.  You may create any number of private methods
 	// if you find them to be helpful.
+	
+	private ColorKey[] colorKeyArray; // array to hold hash table of color keys
+	private static final int DEFAULT_TABLE_SIZE = 61; // default hash table size	
 	
 	public ColorHash(int tableSize, int bitsPerPixel, String collisionResolutionMethod, double rehashLoadFactor){}
 	
