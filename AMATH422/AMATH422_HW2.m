c4fdd4be41215dacaf7e21825a77c02da1082c3d
doerlbh <doerlbh@gmail.com>
2016-11-07 01:08:18 -0800

    update code

---
+++ AMATH422/AMATH422_HW2.m
@@ -0,0 +1,21 @@
+%% HW2 for AMATH422
+% @Author: Baihan Lin
+% @Date: Nov 2016
+
+clear all; close all; clc;
+
+%% I. Dwell time distributions: theory
+
+
+
+
+
+%% II. Simulating Markov Chains and dwell times
+
+
+
+
+
+%% III. Simulating Markov Chains and neural spiking
+
+
