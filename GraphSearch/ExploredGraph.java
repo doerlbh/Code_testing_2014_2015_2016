e433bf7c812051c920e63410ed9655525ed29b12
doerlbh <doerlbh@gmail.com>
2016-11-23 21:26:01 -0800

    update code

--- GraphSearch/ExploredGraph.java
+++ GraphSearch/ExploredGraph.java
@@ -1,14 +1,4 @@
 import java.util.*;
-//
-//import java.util.List;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.Iterator;
-//import java.util.LinkedHashSet;
-//import java.util.Set;
-//import java.util.Stack;
-//import java.util.function.Function;
-
 
 /**
  * @author Baihan Lin  < sunnylin@uw.edu >
@@ -19,7 +9,6 @@ import java.util.*;
  * 
  * Solution to Assignment 5 in CSE 373, Autumn 2016
  * University of Washington.
- * 
  * (Based on starter code v1.3. By Steve Tanimoto.)
  *
  * Java version 8 or higher is recommended.
@@ -31,10 +20,16 @@ public class ExploredGraph {
 	Set<Vertex> Ve; // collection of explored vertices
 	Set<Edge> Ee;   // collection of explored edges
 
-	int nPeg; // Number of pegs
 	LinkedHashMap<String, Vertex> pMap; // predecessors of maps
 	Set<Operator> pMove; // peg moves
 
+	/**
+	 * Constructor to ExploredGraph
+	 * @param Ve stores collection of explored vertices.
+	 * @param Ee stores collection of explored edges.
+	 * @param pMap stores  predecessors of maps
+	 * @param pMove stores peg moves
+	 */
 	public ExploredGraph() {
 		Ve = new LinkedHashSet<Vertex>();
 		Ee = new LinkedHashSet<Edge>();
@@ -43,6 +38,7 @@ public class ExploredGraph {
 	}
 
 	/**
+	 * Testing space
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -57,16 +53,16 @@ public class ExploredGraph {
 
 		// A5E1 Testing 1:
 		// bfs search path for 6 disks, 4 pegs, expect 17
-//		Vertex v0 = eg.new Vertex("[[6,5,4,3,2,1],[],[],[]]");
-//		Vertex v1 = eg.new Vertex("[[],[],[],[6,5,4,3,2,1]]");
-//		System.out.println(v0);
+		//		Vertex v0 = eg.new Vertex("[[6,5,4,3,2,1],[],[],[]]");
+		//		Vertex v1 = eg.new Vertex("[[],[],[],[6,5,4,3,2,1]]");
+		//		System.out.println(v0);
 
 		// A5E1 Testing 2:
 		// bfs search for 4 disks, 5 pegs, expect 7
 		Vertex v0 = eg.new Vertex("[[4,3,2,1],[],[],[],[]]");
 		Vertex v1 = eg.new Vertex("[[],[],[],[],[4,3,2,1]]");
 		System.out.println(v0);
-		
+
 		// find path		
 		eg.initialize(v0);
 		ArrayList<Vertex> path = eg.shortestPath(v0, v1);
@@ -75,6 +71,10 @@ public class ExploredGraph {
 
 	}
 
+	/**
+	 * Public Method to set up an instance of this class
+	 * @param v: the starting vertex v to insert into its set of vertices
+	 */
 	public void initialize(Vertex v) {
 		Ve.add(v);
 		int nPeg = getPegNumber(v);
@@ -87,10 +87,20 @@ public class ExploredGraph {
 		}
 	}
 
+	/**
+	 * Private Method to support initialze and return peg number for A5E1
+	 * @param v: a sample vertex 
+	 * @return peg number
+	 */
 	private int getPegNumber(Vertex v) {
 		return string2peg(v.toString());
 	}
 
+	/**
+	 * Private Method to support getPegNumber and Vertex and return peg number for A5E1
+	 * @param s: a string extracted from a vertex in toString method
+	 * @return peg number
+	 */
 	int string2peg(String s) {
 		int pCount = -1; // account for the first bracket in the front
 		for (char c : s.toCharArray()) {
@@ -102,22 +112,38 @@ public class ExploredGraph {
 		return pCount;
 	}
 
+	/**
+	 * Public Method to obtain the number of vertices
+	 * @return number of vertices
+	 */
 	public int nvertices() {return Ve.size();}
 
+	/**
+	 * Public Method to obtain the number of edges
+	 * @return number of edges
+	 */
 	public int nedges() {return Ee.size();}
 
-	// Iterative Depth-First Search
+	/**
+	 * Public Method to do Iterative Depth-First Search from vi to vj
+	 * @param vi, vj: two vertices
+	 * @return nothing, but added all possible solution into Ve and Ee
+	 */
 	public void idfs(Vertex vi, Vertex vj) {
-		boolean isFound = false;
-		int dist = 0;
-		Stack<Vertex> oVe = new Stack<Vertex>();
-		LinkedList<Vertex> cVe = new LinkedList<Vertex>();
 		pMap.clear();
 		pMap.put(vi.toString(), null);
 		Ee.add(new Edge(null, vi));
+		
+		Stack<Vertex> oVe = new Stack<Vertex>(); //open
+		LinkedList<Vertex> cVe = new LinkedList<Vertex>(); //closed
+		
+		boolean isFound = false;
+		int dist = 0;
+		
 		oVe.push(vi);
+		Vertex curVe; // current Vertex
 		while (!oVe.empty() || !isFound) {
-			Vertex curVe = oVe.pop();
+			curVe = oVe.pop();
 			curVe.dist = dist;
 			dist++;
 			for (Operator move : pMove) {
@@ -125,10 +151,10 @@ public class ExploredGraph {
 				if ( (!nextVe.equals(curVe))
 						&& (oVe.search(nextVe) == -1)
 						&& (!cVe.contains(nextVe))) {
-					oVe.push(nextVe);
-					pMap.put(nextVe.toString(), curVe);
 					Ve.add(nextVe);
 					Ee.add(new Edge(curVe, nextVe));
+					pMap.put(nextVe.toString(), curVe);
+					oVe.push(nextVe);
 				}
 				if (nextVe.equals(vj)) {
 					isFound = true;
@@ -138,19 +164,25 @@ public class ExploredGraph {
 		}
 	}
 
-	// Breadth-First Search
+	/**
+	 * Public Method to do Breadth-First Search from vi to vj
+	 * @param vi, vj: two vertices
+	 * @return nothing, but added all possible solution into Ve and Ee
+	 */
 	public void bfs(Vertex vi, Vertex vj) {
-		boolean endSearch = false;
-		int dist = 0;
-		Queue<Vertex> oVe = new LinkedList<Vertex>();
-		Queue<Vertex> cVe = new LinkedList<Vertex>();
 		pMap.clear();
 		pMap.put(vi.toString(), null);
 		Ee.add(new Edge(null, vi));
+		
+		Queue<Vertex> oVe = new LinkedList<Vertex>(); // open
+		Queue<Vertex> cVe = new LinkedList<Vertex>(); // closed
+		
+		boolean isFound = false;
+		int dist = 0;
+		
 		oVe.add(vi);
-		System.out.println(vi);
-		Vertex curVe;
-		while (!oVe.isEmpty() || !endSearch) {
+		Vertex curVe; // current Vertex
+		while (!oVe.isEmpty() || !isFound) {
 			curVe = oVe.remove();
 			curVe.dist = dist;
 			dist++;
@@ -159,19 +191,24 @@ public class ExploredGraph {
 				if ( (!nextVe.equals(curVe))
 						&& (!oVe.contains(nextVe))
 						&& (!cVe.contains(nextVe))) {
-					oVe.add(nextVe);
-					pMap.put(nextVe.toString(), curVe);
 					Ve.add(nextVe);
 					Ee.add(new Edge(curVe, nextVe));
+					pMap.put(nextVe.toString(), curVe);
+					oVe.add(nextVe);
 				}
 				if (nextVe.equals(vj)) {
-					endSearch = true;
+					isFound = true;
 				}
 			}
 			cVe.add(curVe);
 		}
 	} 
 
+	/**
+	 * Public Method to use path links established to return path
+	 * @param vi: a vertex as target
+	 * @return ArrayList of Vertex which contains states along the path
+	 */
 	public ArrayList<Vertex> retrievePath(Vertex vi) {
 		ArrayList<Vertex> path = new ArrayList<Vertex>();
 		Stack<Vertex> backPath = new Stack<Vertex>();	
